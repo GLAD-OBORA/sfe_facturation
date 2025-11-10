0ad7099445bfe233953426e16ad24104d7481314
GLAD-OBORA <serveursql2@gmail.com>
2025-11-10 18:18:01 +0100

    correction crud admin django allowed correct

--- vinx/settings.py
+++ vinx/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vinxsfe.onrender.com']
+ALLOWED_HOSTS = ['vinxsfev1.onrender.com']
 
 
 # Application definition
